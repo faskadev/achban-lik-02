59725dee208105ea5b228c05ad983e9d89a58327
faskadev <faskadropify@gmail.com>
2026-01-26 00:50:04 +0100

    feat: Update loading timeout in RootLayout and change BASE_URL for API connection

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
-export const BASE_URL = 'http://192.168.1.127:5000';
+export const BASE_URL = 'http://192.168.11.106:5000';
 const API_URL = `${BASE_URL}/api`;
 
 const api = axios.create({
@@ -27,9 +27,7 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    // Handle 401 (Unauthorized) errors globally if needed
     if (error.response && error.response.status === 401) {
-      // You might want to logout the user here
       useAuthStore.getState().logout();
     }
     return Promise.reject(error);
